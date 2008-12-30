dbc50e731629ae9cb5b9453c911b8b83a32140bb
Sean Clark Hess <seanhess@gmail.com>
2008-12-30 14:03:28 -0700

    Changed it to use a flexible host/port

--- start.php
+++ start.php
@@ -1,16 +1,33 @@
 #!/usr/bin/php -q
 <?php  
 
-require_once("SharedServer.php");
+require_once('SharedServer.php');
 
 error_reporting(E_ALL);
+ 
 set_time_limit(0);
+ 
 ob_implicit_flush();   
                    
 $args = $_SERVER['argv'];
 $flags = array(); 
 
-$mySocketServer = new SharedServer();  
+foreach ($args as $arg)
+{
+	if (preg_match('/^-/',$arg))
+		$flags[preg_replace('/^-/','', $arg)] = true;
+}
+
+if (empty($args[1]) || empty($args[2]))
+{
+	echo "\nUsage: start.php HOSTIP PORT (-log)\n";
+	exit();
+}
+
+$ip = $args[1];
+$port = $args[2];  
+
+$mySocketServer = new SharedServer($ip, $port);  
 $mySocketServer->start();
   
 ?>
