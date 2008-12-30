07640ce41be55a841a3eafc617b4230266742f39
Sean Clark Hess <seanhess@gmail.com>
2008-12-30 14:07:13 -0700

    Policy can start on any host

--- policy.php
+++ policy.php
@@ -6,8 +6,18 @@ require_once('light/PolicyServer.php');
 error_reporting(E_ALL);
 set_time_limit(0);
 ob_implicit_flush();   
+
+$args = $_SERVER['argv'];
+
+if (empty($args[1]))
+{
+	echo "\nUsage: policy.php HOST\n";
+	exit();
+}
+
+$host = $args[1];
                    
-$mySocketServer = new PolicyServer('localhost');  
+$mySocketServer = new PolicyServer($host);  
 $mySocketServer->start();
   
 ?>
